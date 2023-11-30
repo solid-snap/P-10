ba84b868261caacc6171a5794e45d880694efc22
VicHoogeveen <92588662+VicHoogeveen@users.noreply.github.com>
2023-11-30 11:13:20 +0100

    database Connectie

--- DbConnect.php
+++ DbConnect.php
@@ -1,1 +1,16 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$dbname = "p10";
+$username = "root";
+$password = "root";
+
+try{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+} catch (PDOException $error){
+    echo $error->getMessage();
+}
