c9c2d10d6329ed16ecbfe0e6fb99b5dcef3b5d95
VicHoogeveen <92588662+VicHoogeveen@users.noreply.github.com>
2023-12-04 11:28:25 +0100

    frontend begin

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <link rel="stylesheet" href="gemeenteStyle.css">
+        <body>
+            <div class="navigatie" id="myTopNav">
+                <nav>
+                    <ul>
+                        <li><img src="image/logo_rotterdam.svg" id=logo alt="TLC Logo" /> </li>
+                        <li><a href="#">Random</a></li>
+                        <li><a href="#">Random</a></li>
+                        <li><a href="#">Kaart</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </body>
+    <footer> Gemeente Rotterdam <br>
+                </footer>
+</html>
+
