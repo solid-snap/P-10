8167d39bb26d3ea6a2ee25fce2f1b1f837b9476c
mau <9014816@student.zadkine.nl>
2024-01-16 13:37:39 +0100

    lepush

--- geolocation/get_marker.php
+++ geolocation/get_marker.php
@@ -1,13 +1,6 @@
 <?php
-$dbnaam = "p10";
-$username = "root";
-$password = "root";
-try{
-    $conn = new PDO("mysql:host=localhost;dbname=$dbnaam",$username, $password);
-}catch(PDOexception $error){
-    echo $error->getMessage();
-}
-
+require "../DbConnect.php";
+global $conn;
 $get_markers = $conn->prepare("SELECT * FROM klacht");
 $get_markers->execute();
 $markers = $get_markers->fetchAll();
