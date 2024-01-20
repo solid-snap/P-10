f4ed0601ea6f19a170e8b884752f089c202bcf2d
maurino <9014816@student.zadkine.nl>
2024-01-20 14:14:33 +0100

    trying

--- geolocation/get_marker.php
+++ geolocation/get_marker.php
@@ -1,9 +1,16 @@
 <?php
+<<<<<<< Updated upstream
 require "../DbConnect.php";
 global $conn;
+=======
+
+require "../DbConnect.php";
+global $conn;
+
+>>>>>>> Stashed changes
 $get_markers = $conn->prepare("SELECT * FROM klacht");
 $get_markers->execute();
 $markers = $get_markers->fetchAll();
 
 //Zorg ervoor dat de response in json wordt geplaatst
-echo json_encode($markers);
+$encoded_data = json_encode($markers);
